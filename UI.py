301f2d226674d2eab8978e41dd7b7685d13a60ca
Rufushwilliams <rhwilliams@perse.co.uk>
2022-08-15 17:41:52 +0100

    MVP Complete

--- UI.py
+++ UI.py
@@ -1,17 +1,21 @@
+from abc import ABC, abstractmethod
 from Game import Game
+import Player
 
-
-class UI:
+class UI(ABC):
     """
-    Basic UI class
+    Basic UI class used to display the game setup to the user
     """
+    def __init__(self, length: int = 4, numGuesses: int = 6, numRounds: int = 1, duplicatesAllowed: bool = False):
+        self._length = length
+        self._numGuesses = numGuesses
+        self._numRounds = numRounds
+        self._duplicatesAllowed = duplicatesAllowed
 
-    def __init__(self, game: Game):
-        self._game = game
-
-    def displayBoard(self):
+    @abstractmethod
+    def run(self):
         """
-        Prints the board
+        Runs the UI
         """
         raise NotImplementedError()
 
@@ -20,10 +24,14 @@ class GUI(UI):
     """
     GUI class that inherits from the UI class
     """
+    def __init__(self, length: int = 4, numGuesses: int = 6, numRounds: int = 1, duplicatesAllowed: bool = False):
+        super().__init__(length, numGuesses, numRounds, duplicatesAllowed)
 
-    def __init__(self, game):
-        super.__init__(self, game)
-        pass
+    def run(self):
+        """
+        Runs the GUI
+        """
+        raise NotImplementedError()
 
 
 class Terminal(UI):
@@ -31,6 +39,88 @@ class Terminal(UI):
     Terminal class that inherits from the UI class
     """
 
-    def __init__(self, game):
-        super.__init__(self, game)
-        pass
+    def __init__(self, length: int = 4, numGuesses: int = 6, numRounds: int = 1, duplicatesAllowed: bool = False):
+        super().__init__(length, numGuesses, numRounds, duplicatesAllowed)
+
+
+    def setup(self):
+        """
+        Sets up the game
+        """
+        print("-------------------------------------------------------")
+        print("Please enter the following information to setup the game")
+        print("-------------------------------------------------------")
+
+        print("How long do you want the code to be? (default 4)")
+        while True:
+            codeLength = input()
+            if codeLength.isdigit():
+                break
+            print("Please enter a valid number")
+        self._length = int(codeLength)
+        print("-------------------------------------------------------")
+        print("How many guesses do you want to have? (default 6)")
+        while True:
+            guesses = input()
+            if guesses.isdigit():
+                break
+            print("Please enter a valid number")
+        self._numGuesses = int(guesses)
+        print("-------------------------------------------------------")
+        print("Do you want to allow duplicates? (y/n) (default n)")
+        while True:
+            duplicates = input()
+            if duplicates.lower() in ["y", "n"]:
+                if duplicates.lower() == "y":
+                    duplicates = True
+                else:
+                    duplicates = False
+                break
+            print("Please enter y or n")
+        self._duplicatesAllowed = duplicates
+        print("-------------------------------------------------------")
+        print("How many rounds do you want to play? (default 1)")
+        while True:
+            rounds = input()
+            if rounds.isdigit():
+                break
+            print("Please enter a valid number")
+        self._numRounds = int(rounds)
+        print("-------------------------------------------------------")
+        print("Game setup complete")
+        print("-------------------------------------------------------")
+
+    def run(self):
+        """
+        Runs the UI
+        """
+        while True:
+            print("-------------------------------------------------------")
+            print("Welcome to Mastermind")
+            print("Enter 1 to play against a computer")
+            print("Enter 2 to play against another human")
+            print("Enter 3 edit game settings")
+            print("Enter 4 to exit")
+            print("-------------------------------------------------------")
+            choice = input("Enter your choice: ")
+            match choice:
+                case "1":
+                    print("You have chosen to play against a computer")
+                    name = input("Please enter your name: ")
+                    player1 = Player.Terminal(name)
+                    player2 = Player.AI("Computer")
+                    game = Game(player1, player2, self._length, self._numGuesses, self._numRounds, self._duplicatesAllowed)
+                    game.run()
+                    continue
+                case "2":
+                    print("You have chosen to play against another human")
+                    raise NotImplementedError()
+                case "3":
+                    self.setup()
+                    continue
+                case "4":
+                    print("Exiting game...")
+                    quit(0)
+                case _:
+                    print("Invalid choice")
+                    continue
