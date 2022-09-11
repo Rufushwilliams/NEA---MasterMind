1bc3bd515832e7cdbcc749dff78d7881036a99bd
Rufushwilliams <rhwilliams@perse.co.uk>
2022-09-11 17:08:15 +0100

    Rewritten the dbm to use a context manager

--- DataBaseManager.py
+++ DataBaseManager.py
@@ -1,45 +1,63 @@
 from __future__ import annotations
+from contextlib import contextmanager
 from dataclasses import dataclass
 from hashlib import sha256
 import sqlite3
 
 
+@contextmanager
+def openDB(db: str) -> sqlite3.Cursor:
+    """
+    A context manager that yields the db cursor and commits the changes.
+    """
+    conn = sqlite3.connect(db)
+    try:
+        cur = conn.cursor()
+        yield cur
+    finally:
+        conn.commit()
+        conn.close()
+
+
 class dataBaseManager:
-    ################################################
-    # TODO: REWRITE THIS CLASS TO USE WITH KEYWORD #
-    ################################################
+    """
+    A class that contains functions required to interact with the statistics database
+    """
+
     def __init__(self, db: str):
-        self.conn = sqlite3.connect(db)
-        self.cur = self.conn.cursor()
-        self.cur.execute(
-            """
-        CREATE TABLE IF NOT EXISTS users (
-            username text PRIMARY KEY,
-            passwordHash text,
-            wins int DEFAULT 0,
-            losses int DEFAULT 0,
-            draws int DEFAULT 0,
-            totalGames int DEFAULT 0,
-            roundsPlayed int DEFAULT 0,
-            timePlayed float DEFAULT 0
-            )"""
-        )
-        self.conn.commit()
+        self.db = db
+        # create the database if it doesn't exist
+        with openDB(self.db) as cur:
+            cur.execute(
+                """
+            CREATE TABLE IF NOT EXISTS users (
+                username text PRIMARY KEY,
+                passwordHash text,
+                wins int DEFAULT 0,
+                losses int DEFAULT 0,
+                draws int DEFAULT 0,
+                totalGames int DEFAULT 0,
+                roundsPlayed int DEFAULT 0,
+                timePlayed float DEFAULT 0
+                )"""
+            )
 
     def register(self, username: str, password: str) -> bool:
         """
         Takes a username and password and adds them to the database.
         Returns True if the registration works, False if the username is already taken
         """
+        # hash the password
         hashedPassword = sha256(password.encode()).hexdigest()
         try:
-            self.cur.execute(
-                "INSERT INTO users (username, passwordHash) VALUES (?, ?)",
-                (username, hashedPassword),
-            )
-            self.conn.commit()
+            with openDB(self.db) as cur:
+                # insert the username and password hash into the database
+                cur.execute(
+                    "INSERT INTO users (username, passwordHash) VALUES (?, ?)",
+                    (username, hashedPassword),
+                )
             return True
-        except sqlite3.IntegrityError:
+        except sqlite3.IntegrityError:  # if the username is already taken
             return False
 
     def login(self, username: str, password: str) -> bool:
@@ -49,12 +67,13 @@ class dataBaseManager:
         # hash the password
         hashedPassword = sha256(password.encode()).hexdigest()
         # select the password hash from the database where the username is the one entered
-        self.cur.execute(
-            "SELECT passwordHash FROM users WHERE username = ?", (username,)
-        )
-        # get the password hash from the database
-        result = self.cur.fetchone()
-        if result and result[0] == hashedPassword:
+        with openDB(self.db) as cur:
+            cur.execute(
+                "SELECT passwordHash FROM users WHERE username = ?", (username,)
+            )
+            # fetch the password hash
+            passwordHash = cur.fetchone()
+        if passwordHash and passwordHash[0] == hashedPassword:
             # if the password hashes match, return True
             return True
         return False
@@ -63,12 +82,13 @@ class dataBaseManager:
         """
         Reads the statistics for the user from the database
         """
-        self.cur.execute(
-            """SELECT username, wins, losses, draws, totalGames, roundsPlayed, timePlayed FROM users WHERE username = ?""",
-            (username,),
-        )
-        stats = Statistics(*self.cur.fetchone())
-        return stats
+        with openDB(self.db) as cur:
+            cur.execute(
+                "SELECT wins, losses, draws, totalGames, roundsPlayed, timePlayed FROM users WHERE username = ?",
+                (username,),
+            )
+            stats = cur.fetchone()
+        return Statistics(*stats)
 
     def createEmptyStatsTable(self, username: str) -> Statistics:
         """
@@ -77,22 +97,19 @@ class dataBaseManager:
         return Statistics(username)
 
     def saveStatsTable(self, stats: Statistics):
-        self.cur.execute(
-            """UPDATE users SET wins = ?, losses = ?, draws = ?, totalGames = ?, roundsPlayed = ?, timePlayed = ? WHERE username = ?""",
-            (
-                stats.wins,
-                stats.losses,
-                stats.draws,
-                stats.totalGames,
-                stats.roundsPlayed,
-                stats.timePlayed,
-                stats.username,
-            ),
-        )
-        self.conn.commit()
-
-    def __del__(self):
-        self.conn.close()
+        with openDB(self.db) as cur:
+            cur.execute(
+                """UPDATE users SET wins = ?, losses = ?, draws = ?, totalGames = ?, roundsPlayed = ?, timePlayed = ? WHERE username = ?""",
+                (
+                    stats.wins,
+                    stats.losses,
+                    stats.draws,
+                    stats.totalGames,
+                    stats.roundsPlayed,
+                    stats.timePlayed,
+                    stats.username,
+                ),
+            )
 
 
 @dataclass
